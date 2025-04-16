300bca29079f891effbc8d4797ae3c79e97e43e4
Artyom Zabroda <artyomzabroda@gmail.com>
2025-04-16 13:57:33 +0300

    String literals

--- tests/scanner_test.c
+++ tests/scanner_test.c
@@ -95,12 +95,182 @@ void TestUnterminatedCharConst(void **state) {
   struct TestState *teststate = *state;
   const char source[] = "'";
   
-  int i;
   struct Token tok;
 
   expect_function_call(__wrap_DiagReport);
   expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_UNTERMINATED_CHAR_CONST);
   ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag);
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestSingleBackslashCharConst(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "'\\'";
+  struct Token tok;
+
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_UNTERMINATED_CHAR_CONST);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag);
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestEmptyCharConst(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "''";
+  struct Token tok;
+
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_EMPTY_CHAR_CONST);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag);
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestInvalidEscapeSequenceInCharConst(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "'\\w'";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_INCORRECT_ESCAPE_SEQUENCE);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestInvalidHexEscapeSequenceInCharConst(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "'\\x'";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_INCORRECT_HEX_ESCAPE_SEQUENCE);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestWideCharUnsupportedInCharConst(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "L''";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_WIDE_CHAR_CONSTANTS_UNSUPPORTED);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestIdentifierThatStartsWithL(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "La'";
+  struct Token tok;
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_IDENTIFIER);
+}
+
+void TestNullByteInFileIsIgnoredWithWarning(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "abc\0def";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_NULL_IN_FILE);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_IDENTIFIER);
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_IDENTIFIER);
+}
+
+void TestNullByteInCharConst(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "\'\0\'";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_UNTERMINATED_CHAR_CONST);
+
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+  expect_function_calls(__wrap_DiagReport, 2);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_NULL_IN_FILE);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_UNTERMINATED_CHAR_CONST);
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestNullByteInStringLiteral(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "\"\0\"";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_UNTERMINATED_STRING_LITERAL);
+
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+  expect_function_calls(__wrap_DiagReport, 2);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_NULL_IN_FILE);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_UNTERMINATED_STRING_LITERAL);
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestValidStringLiterals(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "\"\" \"abc\" \"\\'\" \"\\\"\" \"\\\\\" \"\\a\" \"\\b\" \"\\f\" \"\\n\" \"\\r\" \"\\t\" \"\\v\" \"\\x1\" \"\\x12\" \"\\0\" \"'\"";
+  struct Token tok;
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  while ((tok = Scan(&teststate->scanner)).type != TOKEN_TYPE_EOF) {
+    assert_int_equal(tok.type, TOKEN_TYPE_STRING_LITERAL);
+  }
+}
+
+void TestSingleBackSlashInStringLiteral(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "\"\\\"";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_UNTERMINATED_STRING_LITERAL);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestInvalidHexEscapeSequenceInStringLiteral(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "\"\\x\"";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_INCORRECT_HEX_ESCAPE_SEQUENCE);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestInvalidEscapeSequenceInStringLiteral(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "\"\\w\"";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_INCORRECT_ESCAPE_SEQUENCE);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
+  tok = Scan(&teststate->scanner);
+  assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
+}
+
+void TestWideCharUnsupportedStringLiteral(void **state) {
+  struct TestState *teststate = *state;
+  const char source[] = "L\"abc\"";
+  struct Token tok;
+  expect_function_call(__wrap_DiagReport);
+  expect_value(__wrap_DiagReport, diag_type, DIAG_TYPE_WIDE_STRING_LITERALS_UNSUPPORTED);
+  ScannerInit(&teststate->scanner, source, sizeof(source), &teststate->diag); 
   tok = Scan(&teststate->scanner);
   assert_int_equal(tok.type, TOKEN_TYPE_UNKNOWN);
 }
@@ -112,6 +282,20 @@ int main(void) {
       cmocka_unit_test(TestValidPPNumbers),
       cmocka_unit_test(TestValidCharConsts),
       cmocka_unit_test(TestUnterminatedCharConst),
+      cmocka_unit_test(TestSingleBackslashCharConst),
+      cmocka_unit_test(TestEmptyCharConst),
+      cmocka_unit_test(TestInvalidEscapeSequenceInCharConst),
+      cmocka_unit_test(TestInvalidHexEscapeSequenceInCharConst),
+      cmocka_unit_test(TestWideCharUnsupportedInCharConst),
+      cmocka_unit_test(TestIdentifierThatStartsWithL),
+      cmocka_unit_test(TestNullByteInFileIsIgnoredWithWarning),
+      cmocka_unit_test(TestNullByteInCharConst),
+      cmocka_unit_test(TestNullByteInStringLiteral),
+      cmocka_unit_test(TestValidStringLiterals),
+      cmocka_unit_test(TestSingleBackSlashInStringLiteral),
+      cmocka_unit_test(TestInvalidHexEscapeSequenceInStringLiteral),
+      cmocka_unit_test(TestInvalidEscapeSequenceInStringLiteral),
+      cmocka_unit_test(TestWideCharUnsupportedStringLiteral)
   };
   return cmocka_run_group_tests(scanner_tests, ScannerGroupSetup, ScannerGroupTeardown);
 }
