e79542f38ab5b9384870f4e3cb235dbb4410193c
Artyom Zabroda <artyomzabroda@gmail.com>
2025-04-24 21:13:05 +0300

    Starting to rewrite in c++

--- src/common/crash.h
+++
@@ -1,6 +0,0 @@
-#ifndef CRASH_H_
-#define CRASH_H_
-
-void Crash(int exit_code, const char *const format, ...);
-
-#endif
--- src/frontend/parser.h
+++
@@ -1,7 +0,0 @@
-#ifndef FRONTEND_PARSER_H_
-#define FRONTEND_PARSER_H_
-
-#include "buffered_file.h"
-
-
-#endif
