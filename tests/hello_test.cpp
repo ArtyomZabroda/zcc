195c0fbec17ec113fc4c7cd84ecda1f4c299463f
Artyom Zabroda <artyomzabroda@gmail.com>
2025-04-24 22:29:19 +0300

    Added vcpkg and gtest

---
+++ tests/hello_test.cpp
@@ -0,0 +1,21 @@
+#include <gtest/gtest.h>
+
+int foo_add(int a, int b) {
+  return a + b;
+}
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions2) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(foo_add(1, 1), 2);
+}
